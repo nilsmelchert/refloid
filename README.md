f7fe00aa722750500a2bee2205a117bd3a9b767f
Nils Melchert <nils.melchert@imr.uni-hannover.de>
2019-04-29 17:27:26 +0200

    Added rt_object class

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
  This Repo contains a CMakeList, which is able to include and link everything that is needed from the NVIDIA-Optix-6.0 Toolkit in order to use that framework for your own purposes.
  
+Dependencies:
+    - Optix 6.0
+    - Qt5 (From apt tree)
--- nslaift_integration/main.cpp
+++ nslaift_integration/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
-#include <cstdio>
+#include <QCoreApplication>
 #include "spdlog/spdlog.h"
 
+#include "src/host/RT_object.h"
+
+#include <optix.h>
+#include <optixu/optixpp_namespace.h>
+#include <optixu/optixu_math_stream_namespace.h>
+#include <optixu_math_namespace.h>
 int main(){
+    spdlog::set_level(spdlog::level::debug);
     spdlog::info("Starting raytracing test application");
+//    optix::Matrix4x4 trans = optix::Matrix4x4::identity();
+//    trans = trans.translate(optix::make_float3(1,2,3));
+    optix::Matrix4x4 trans = optix::Matrix4x4::translate(optix::make_float3(1,2,3));
+    trans *= optix::Matrix4x4::translate(optix::make_float3(1,2,3));
+    std::cout << trans[3] << std::endl;
+    std::cout << trans[7] << std::endl;
+    std::cout << trans[11] << std::endl;
+
+    return 0;
 }
